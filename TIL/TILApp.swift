3a0ad8f85d7ff5adfa147d7818faa2c5fc9c077a
roco <roco10000hours@gmail.com>
2022-10-01 13:56:43 +0900

    Initial Commit

---
+++ TIL/TILApp.swift
@@ -0,0 +1,17 @@
+//
+//  TILApp.swift
+//  TIL
+//
+//  Created by 앙겔로스 on 2022/10/01.
+//
+
+import SwiftUI
+
+@main
+struct TILApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
